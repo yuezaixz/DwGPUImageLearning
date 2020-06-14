c0a450f9262cc69218e6761aeb73f434842cef73
yuezaixz <xiao303178394@gmail.com>
2020-06-14 11:35:29 +0800

    update

--- DwGPUImageLearning/Example/StillCamera/StillCameraToLibaryViewController.swift
+++ DwGPUImageLearning/Example/StillCamera/StillCameraToLibaryViewController.swift
@@ -19,6 +19,7 @@ class StillCameraToLibaryViewController: UIViewController {
         
     var filterOperation: FilterOperationInterface?
     @IBOutlet weak var saveToLibaryButton: UIButton!
+    @IBOutlet weak var saveImageView: UIImageView!
     
     required init(coder aDecoder: NSCoder)
     {
@@ -51,6 +52,7 @@ class StillCameraToLibaryViewController: UIViewController {
             self.present(errorAlertController, animated: true, completion: nil)
             return
         }
+        videoCamera.delegate = self
         if let currentFilterConfiguration = self.filterOperation {
             self.title = currentFilterConfiguration.titleName
             
@@ -82,25 +84,38 @@ class StillCameraToLibaryViewController: UIViewController {
     }
 
     @IBAction func saveToLibaryAction(_ sender: Any) {
-        if let currentFilterConfiguration = self.filterOperation {
-            saveToLibaryButton.isEnabled = false
+        if let image = lastImg {
+            saveImageView.image = image
             
-            let pictureOutput = PictureOutput()
-            pictureOutput.encodedImageFormat = .jpeg
-            pictureOutput.encodedImageAvailableCallback = { imageData in
-                print(imageData)
-                if let image = UIImage(data: imageData) {
-                    PHPhotoLibrary.shared().performChanges({
-                        PHAssetChangeRequest.creationRequestForAsset(from: image)
-                    }) { (isSuccess, error) in
-                        if isSuccess {
-                            print("保存成功")
-                        }
-                    }
-                }
-            }
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+            
+//            PhotoAlbumUtil.saveImageInAlbum(image: image, albumName: "GPUImage") { (result) in
+//                switch result{
+//                case .success:
+//                    print("保存成功")
+//                case .denied:
+//                    print("被拒绝")
+//                case .error:
+//                    print("保存错误")
+//                }
+//            }
         }
     }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            print("Saved!")
+//            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "OK", style: .default))
+//            present(ac, animated: true)
+        }
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         if let videoCamera = videoCamera {
             videoCamera.stopCapture()
@@ -110,4 +125,17 @@ class StillCameraToLibaryViewController: UIViewController {
         
         super.viewWillDisappear(animated)
     }
+    
+    private var lastImg: UIImage?
+}
+
+extension StillCameraToLibaryViewController: CameraDelegate {
+    func didCaptureBuffer(_ sampleBuffer: CMSampleBuffer) {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            let attachments = CMCopyDictionaryOfAttachments(allocator: kCFAllocatorDefault, target: sampleBuffer, attachmentMode: CMAttachmentMode(kCMAttachmentMode_ShouldPropagate))!
+            let img = CIImage(cvPixelBuffer: pixelBuffer, options: attachments as? [CIImageOption: Any])
+            lastImg = UIImage(ciImage: img)
+        }
+    }
+    
 }
